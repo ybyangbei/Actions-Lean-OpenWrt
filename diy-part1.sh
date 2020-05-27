85e207cc7535d01556345258ca0215e4241a12db
lxhao61 <751142257@qq.com>
2020-05-27 13:34:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# comment a feed source
+sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
