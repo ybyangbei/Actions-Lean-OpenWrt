7904169a9084c118fadafc5251c96ef51cd80cd5
lxhao61 <751142257@qq.com>
2020-06-21 22:57:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,17 +31,17 @@ rm -rf feeds/telephony/net/freeswitch-stable-mod-bcg729
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/lienol/smartdns
 
 # lienol大的passwall插件及依赖
-svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/lienol/brook
-svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/lienol/chinadns-ng
-svn co https://github.com/Lienol/openwrt-package/trunk/package/dns2socks package/lienol/dns2socks
-svn co https://github.com/Lienol/openwrt-package/trunk/package/ipt2socks package/lienol/ipt2socks
-svn co https://github.com/Lienol/openwrt-package/trunk/package/kcptun package/lienol/kcptun
-svn co https://github.com/Lienol/openwrt-package/trunk/package/openssl1.1 package/lienol/openssl1.1
-svn co https://github.com/Lienol/openwrt-package/trunk/package/pdnsd-alt package/lienol/pdnsd-alt
-svn co https://github.com/Lienol/openwrt-package/trunk/package/shadowsocksr-libev package/lienol/shadowsocksr-libev
-svn co https://github.com/Lienol/openwrt-package/trunk/package/simple-obfs package/lienol/simple-obfs
-svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/lienol/tcping
-svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan package/lienol/trojan
-svn co https://github.com/Lienol/openwrt-package/trunk/package/v2ray-plugin package/lienol/v2ray-plugin
-svn co https://github.com/Lienol/openwrt-package/trunk/package/v2ray package/lienol/v2ray
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/lienol/luci-app-passwall
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/lienol/brook
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/lienol/chinadns-ng
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/dns2socks package/lienol/dns2socks
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/ipt2socks package/lienol/ipt2socks
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/kcptun package/lienol/kcptun
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/openssl1.1 package/lienol/openssl1.1
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/pdnsd-alt package/lienol/pdnsd-alt
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/shadowsocksr-libev package/lienol/shadowsocksr-libev
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/simple-obfs package/lienol/simple-obfs
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/lienol/tcping
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan package/lienol/trojan
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/v2ray-plugin package/lienol/v2ray-plugin
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/v2ray package/lienol/v2ray
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/lienol/luci-app-passwall
